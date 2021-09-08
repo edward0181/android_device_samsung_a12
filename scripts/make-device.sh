9b6533ca6938e79d5a27fc13dadfa5467dbbc76f
edward0181 <edwardroggeveen@gmail.com>
2021-09-08 14:38:29 +0200

    Update make-device.sh

--- scripts/make-device.sh
+++ scripts/make-device.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
-set -ev
-#installing needed appts
-curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-chmod a+x ~/bin/repo
-echo "export PATH=~/bin:$PATH" >> ~/.bashrc
-source ~/.bashrc
-# Create dirs
-mkdir tw; cd tw
+#!bin/bash
 
-# Init repo
+# Go to the working directory
+mkdir ~/TWRP && cd ~/TWRP
+# Configure git
+git config --global user.email "edwardroggeveen@gmail.com"
+git config --global user.name "edward0181"
+# Sync the source
 repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11
-
-# Clone a12 repo
+repo sync  -f --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
+# Clone device tree and common tree
 git clone https://github.com/edward0181/android_device_samsung_a12 -b twrp-11.0 device/samsung/a12
-
-# Sync
-repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j`nproc`
-
-# Build
-source build/envsetup.sh; export ALLOW_MISSING_DEPENDENCIES=true; lunch twrp_a12-eng; mka recoveryimage
+# Build recovery image
+export ALLOW_MISSING_DEPENDENCIES=true; . build/envsetup.sh; lunch twrp_a12-eng; make -j$(nproc --all) recoveryimage
+# Rename and copy the files
+twrp_version=$(cat ~/TWRP/bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d '"' -f2)
+date_time=$(date +"%d%m%Y%H%M")
+mkdir ~/final
+cp recovery.img ~/final/twrp-$twrp_version-a12-"$date_time"-unofficial.img
