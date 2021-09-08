ddc42ffbceaa427c180260690e0f2847dde144f8
edward0181 <edwardroggeveen@gmail.com>
2021-09-08 13:47:40 +0200

    Update make-device.sh

--- scripts/make-device.sh
+++ scripts/make-device.sh
@@ -5,17 +5,18 @@ curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/rep
 chmod a+x ~/bin/repo
 echo "export PATH=~/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
+sudo apt install python3
 # Create dirs
 mkdir tw; cd tw
 
 # Init repo
-repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11
+python3 ~/bin/repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11
 
 # Clone a12 repo
 git clone https://github.com/edward0181/android_device_samsung_a12 -b twrp-11.0 device/samsung/a12
 
 # Sync
-repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j`nproc`
+python3 ~/bin/repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j`nproc`
 
 # Build
 source build/envsetup.sh; export ALLOW_MISSING_DEPENDENCIES=true; lunch twrp_a12-eng; mka recoveryimage
