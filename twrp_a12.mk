b5d471eae80cdb13abbb5d6fb77d39f52a9a2c64
edward0181 <edwardroggeveen@gmail.com>
2021-09-08 09:13:59 +0200

    Cleaning mess

--- twrp_a12.mk
+++ twrp_a12.mk
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 # Release name
 PRODUCT_RELEASE_NAME := a12
 
@@ -23,22 +24,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Include any options that can't be included in BoardConfig.mk
-$(call inherit-product, device/samsung/a12/device.mk)
 
-#PRODUCT_SHIPPING_API_LEVEL := 29
-
-# Dynamic Partition
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
 
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
-
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := twrp_a12
@@ -46,4 +42,8 @@ PRODUCT_DEVICE := a12
 PRODUCT_MODEL := SM-A125F
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
+TW_DEVICE_VERSION := Edward0181-07092021
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31 \
