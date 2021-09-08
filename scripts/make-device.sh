92488178a2032ba2f82c3cd53945368a3fc7898f
edward0181 <edwardroggeveen@gmail.com>
2021-09-08 13:29:16 +0200

    Update make-device.sh

--- scripts/make-device.sh
+++ scripts/make-device.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 #installing needed appts
-sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5-dev lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
+sudo apt-get install git ccache automake lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng
 # Create dirs
 mkdir tw; cd tw
 
