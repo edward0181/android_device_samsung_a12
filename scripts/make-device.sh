9b853154b064cd679974bca4f58e831ef2dc5a7d
edward0181 <edwardroggeveen@gmail.com>
2021-09-08 13:32:39 +0200

    Update make-device.sh

--- scripts/make-device.sh
+++ scripts/make-device.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -ev
 #installing needed appts
-sudo apt-get install git ccache automake lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng
+mkdir ~/bin
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+source ~/.bashrc
 # Create dirs
 mkdir tw; cd tw
 
