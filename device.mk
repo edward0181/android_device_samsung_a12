541ee0d33f9346a3c4cd3d20be01d0be5145a1bc
edward0181 <edwardroggeveen@gmail.com>
2021-10-04 17:02:37 +0200

    Latest updates on FSTAB, and MTP

--- device.mk
+++ device.mk
@@ -19,7 +19,13 @@ LOCAL_PATH := device/samsung/a12
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
