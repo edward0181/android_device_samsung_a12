9d9f4a4532170bf835758d5bcbe4af5afa4e232b
edward0181 <edwardroggeveen@gmail.com>
2021-10-06 12:51:45 +0200

    Revert "initial Lineage-OS"

---
+++ twrp_a12.mk
@@ -0,0 +1,41 @@
+ 
+#
+# Copyright (C) 2021 Durasame
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Release name
+PRODUCT_RELEASE_NAME := a12
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Include any options that can't be included in BoardConfig.mk
+$(call inherit-product, device/samsung/a12/device.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a12/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_a12
+PRODUCT_DEVICE := a12
+PRODUCT_MODEL := SM-A125F
+PRODUCT_BRAND := Samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
