2dc043dca03982d3ab135487990c2d5451d0bc03
edward0181 <edwardroggeveen@gmail.com>
2022-04-12 09:44:54 +0200

    Rebuild to A125FXXU2BVB4 source
    Rebuild whole tree with A125FXXU2BVB4 firmware.
    Kernel is custom build. DTB and DTBO from A125FXXU2BVB4 firmware.

---
+++ README.md
@@ -0,0 +1,31 @@
+## Recovery Device Tree for the Samsung Galaxy A12 (MTK)
+
+## How-to compile it:
+
+# Create dirs
+$ mkdir tw; cd tw
+
+# Init repo
+$ repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11
+
+# Clone a12 repo
+$ git clone https://github.com/edward0181/android_device_samsung_a12 -b twrp-11.0 device/samsung/a12
+
+# Clone a12 kernel
+$ git clone https://github.com/00p513-dev/android_kernel_samsung_a12.git kernel/samsung/a12
+
+# Sync
+$ repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j`nproc`
+
+# Build
+$ source build/envsetup.sh; export ALLOW_MISSING_DEPENDENCIES=true; lunch twrp_a12-eng; mka recoveryimage
+
+# Disable File Based Encryption (FBE) after installing TWRP.
+$ Boot TWRP; format DATA partition; start TWRP SHELL; execute: multidisabler.
+Your DATA partition will be secured against re-encryption.
+
+
+Blobs version:
+> Kernel base: Compiled from source.
+> Ramdisk, DTB, DTBO base: A125FXXU2BVB4
+
---
+++ twrp_a12.mk
@@ -0,0 +1,42 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+DEVICE_PATH := device/samsung/a12
+
+# Release name
+PRODUCT_RELEASE_NAME := a12
+
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_a12
+PRODUCT_DEVICE := a12
+PRODUCT_MODEL := SM-A125F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
